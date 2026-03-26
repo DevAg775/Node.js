abdbb7bdb28aadd334e8135d7ab0e32efbcaa2bf
DevAg775 <devanshagrawal775@gmail.com>
2026-03-27 01:17:48 +0530

    Understanding about URL

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 //Server
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myServer =http.createServer((req,res)=> {
+    if(req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myurl = url.parse(req.url,true)
+    console.log(myurl);
     fs.appendFile("log.txt",log,(err,data)=>{
-    switch(req.url){
+    switch(myurl.pathname){
         case '/': res.end("Homepage");
         break
-        case '/about': res.end("Hello My name is devansh");
+        case '/about': 
+        const username = myurl.query.myname
+        res.end(`Hi,${username}`);
+        break
+        case '/search':
+        const search = myurl.query.search_query;
+        res.end('Here are your results for '+search)
         break
         case '/register': res.end("You want to register huh?");
         break
